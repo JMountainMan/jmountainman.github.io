ed43a9bdada69d28e904be8a74eaa6382b1552e4
JMountainMan <johann_bergmann@yahoo.de>
2017-05-22 20:45:24 +0200

    yes

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,21 +4,6 @@ $(document).ready(function() {
     });
 });
 
-// Hauptklasse trifft auf top
-/* Lösungsvorschlag / Ideen: */
-$(document).on('scroll', function() {
-    var lifeTop = $('.lifestyle').offset().top;
-    var eduTop = $('.education').offset().top;
-
-    if ($(window).scrollTop() >= lifeTop) {
-        console.log("Hit Lifestyle");
-    }
-
-    if ($(window).scrollTop() >= eduTop) {
-        console.log("Hit Education");
-    }
-});
-
 $.urlParam = function(name){
 	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
 	return results[1] || 0;
